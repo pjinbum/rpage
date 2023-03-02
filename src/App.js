29b76bce4480b1f30fccab9b111537100d3e53d8
pjinbum <119594529+pjinbum@users.noreply.github.com>
2023-03-02 11:34:36 +0900

    🔥 opop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 /*eslint-disable*/
 import logo from './logo.svg';
 import './App.css';
+// import './'
 
 function App() {
   return (
     <div className="App">
-     <h1></h1>
+     <h1>동해물과 백두산이 마르고 닳도록</h1>
+     <div className='box'>하하하</div>
+     <input type="text" />
+     <img src="" alt="" />
+     {/* 셀프 클로징 */}
     </div>
+
   );
 }
 
