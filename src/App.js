35d876335ec1c83fcc3e0c11ee2499d014d035fb
pjinbum <119594529+pjinbum@users.noreply.github.com>
2023-03-02 11:00:48 +0900

    ⚡️ rarara

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1></h1>
+    </div>
+  );
+}
+
+export default App;
